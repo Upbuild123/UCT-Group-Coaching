0953160f1f1ce3a150c7c70978a547f245f0511f
Michael Sloyer <michaelsloyer@MacBook-Pro-8.local>
2026-06-12 12:23:31 +0900

    fix: remove Hawaii timezone from timezone list

--- lib/timezones.ts
+++ lib/timezones.ts
@@ -3,7 +3,6 @@ export const TIMEZONES = [
   { label: 'Central (CT)', value: 'America/Chicago' },
   { label: 'Mountain (MT)', value: 'America/Denver' },
   { label: 'Pacific (PT)', value: 'America/Los_Angeles' },
-  { label: 'Hawaii (HT)', value: 'Pacific/Honolulu' },
   { label: 'London (GMT/BST)', value: 'Europe/London' },
   { label: 'Paris / Berlin (CET)', value: 'Europe/Paris' },
   { label: 'India (IST)', value: 'Asia/Kolkata' },
